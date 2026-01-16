37d6b2bb70fb20695cd880b5e92b98e49236eb16
BlauRick <157022879+BlauRick@users.noreply.github.com>
2026-01-16 12:14:31 +0100

    Update main_db_procedures.sql

--- db/main_db_procedures.sql
+++ db/main_db_procedures.sql
@@ -9,3 +9,15 @@ END;
 //
 DELIMITER ;
 
+DELIMITER //
+CREATE OR REPLACE PROCEDURE ordayna_main_db.getOrphanedIntezmenys()
+BEGIN
+	SELECT intezmeny_id
+	FROM intezmeny_ids
+	LEFT JOIN intezmeny_ids_users
+	ON intezmeny_id=intezmeny_ids_id
+	GROUP BY intezmeny_id
+	HAVING COUNT(if(is_admin>0,1,NULL))=0;
+END;
+//
+DELIMITER ;
